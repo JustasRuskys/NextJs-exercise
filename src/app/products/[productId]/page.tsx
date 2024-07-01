63efdbdb1c63ad583d9215be7ccd10e329370546
JustasRuskys <justas@expertmedia.lt>
2024-07-01 14:16:49 +0300

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,22 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: String;
+    };
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
 const ProductDetails = ( { params }: {
     params: {productId: string};
 } ) => {
